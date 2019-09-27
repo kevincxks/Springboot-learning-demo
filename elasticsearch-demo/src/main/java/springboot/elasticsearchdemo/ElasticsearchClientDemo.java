afbe06e2190383873e3866482f5f1162d112f688
薛凯元 <1162367203@qq.com>
2019-09-27 22:25:38 +0800

    test

--- elasticsearch-demo/src/main/java/springboot/elasticsearchdemo/ElasticsearchClientDemo.java
+++ elasticsearch-demo/src/main/java/springboot/elasticsearchdemo/ElasticsearchClientDemo.java
@@ -8,6 +8,10 @@ import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ElasticsearchClientDemo {
     private static String CLUSTER_NAME = "elasticsearch_xuekaiyuan";
@@ -15,14 +19,46 @@ public class ElasticsearchClientDemo {
     private static int TCP_PORT = 9300;
 
     public static void main(String[] args) throws UnknownHostException {
-        Settings settings = Settings.builder()
-                .put("cluster.name",CLUSTER_NAME)
-                .build();
-        TransportClient client = new PreBuiltTransportClient(settings)
-                .addTransportAddress(new TransportAddress(InetAddress.getByName(HOST_IP),TCP_PORT));
-        GetResponse getResponse = client.prepareGet("books","IT","1").get();
-        System.out.println(getResponse.getSourceAsString());
+        List<Integer> collect = Arrays.stream(new int[]{2, 3, 4, 1, 2, 4, 5, 7, 4, 2, 1, 2, 5}).mapToObj(Integer::new).collect(Collectors.toList());
+        mySort(collect,0,collect.size()-1);
+        System.out.println(collect);
     }
+
+    public static void mySort(List<Integer> list,int begin,int end){
+        if(begin>=end) return;
+        int tmp = partition(list,begin,end);
+        mySort(list,begin,tmp-1);
+        mySort(list,tmp+1,end);
+    }
+
+    public static int partition(List<Integer> list,int begin,int end){
+        if(begin==end) return begin;
+        int tmp = list.get(begin);
+        int i =begin,j=end;
+        while(i>=begin&&j<=end){
+            while(i<=end&&list.get(i)<=tmp){
+                i++;
+            }
+            while(j>=begin&&list.get(j)>tmp){
+                j--;
+            }
+            if(j<i){
+                int tt = list.get(j);
+                list.set(begin,tt);
+                list.set(j,tmp);
+                return j;
+            }
+
+            int t = list.get(i);
+            list.set(i,list.get(j));
+            list.set(j,t);
+            i++;
+            j--;
+
+        }
+        return -1;
+    }
+
 
 
 }
